384f40b6a068af9fad8ca2c4c165ca908c981756
Efe <efeserdar123123@gmail.com>
2026-01-01 16:54:07 +0300

    Fix: html unescape and scoring logic

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+import html
 from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
+
 question_bank = []
-point = 0
-QuestionNumber = 0
+
 for data in question_data:
     for question in data["results"]:
-        question_text = question["question"]
+        question_text = html.unescape(question["question"]) 
         question_answer = question["correct_answer"]
-        question_object = Question(question_text, question_answer)
-        question_bank.append(question_object)
+        
+        new_question = Question(question_text, question_answer)
+        question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
+score = 0
 
 while quiz.still_has_questions():
     user_answer, correct_answer = quiz.next_question()
-    QuestionNumber += 1
-    if user_answer == correct_answer.lower():
-        point += 1
-        print("Correct! You have +1 point.")
+    
+    if user_answer.lower() == correct_answer.lower():
+        score += 1
+        print(f"Correct! Your current score is: {score}/{quiz.QuestionNumber}")
     else:
         print(f"Incorrect! The correct answer was: {correct_answer}")
-print("Your total point is", point)
+    print("\n")
+
+print(f"Quiz completed! Your final score is: {score}/{len(question_bank)}")
